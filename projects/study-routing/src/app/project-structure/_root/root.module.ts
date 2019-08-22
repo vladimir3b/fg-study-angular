bfb37abbc1db54bd450abf70f23d7485793802eb
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2019-08-22 13:29:35 +0300

    starting study-routing

---
+++ projects/study-routing/src/app/project-structure/_root/root.module.ts
@@ -0,0 +1,36 @@
+import { RootRouter } from './root.router';
+import { NavigationService } from './services/navigation.service';
+import { SharedModules } from './../../../../../.shared-resources/modules/shared.module';
+import { RootComponent } from './components/root/root.component';
+import { NgModule } from '@angular/core';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainNavigationComponent } from './components/main-navigation/main-navigation.component';
+import { SidebarNavigationComponent } from './components/sidebar-navigation/sidebar-navigation.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [
+    RootComponent,
+    PageNotFoundComponent,
+    FooterComponent,
+    HomeComponent,
+    SidebarComponent,
+    MainNavigationComponent,
+    SidebarNavigationComponent,
+    MainHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModules,
+    RootRouter
+  ],
+  providers: [
+    NavigationService
+  ],
+  bootstrap: [ RootComponent ]
+})
+export class RootModule { }
